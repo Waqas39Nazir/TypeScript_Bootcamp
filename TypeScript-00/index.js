b4368cca5387aa004a318b310974e49dda6e4bbc
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-22 16:48:35 +0500

    Initial Commit

--- TypeScript-00/index.js
+++ TypeScript-00/index.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("I will learn what is need to be learned ABC");
+console.log("I will learn what is need to be learned ABC DEF");
 /** If not using nodemon
  * transpile JavaScript tsc
  * run transpiled javascript by running the following command node index.ts
--- TypeScript-00/index.ts
+++ TypeScript-00/index.ts
@@ -1,4 +1,4 @@
-console.log("I will learn what is need to be learned ABC");
+console.log("I will learn what is need to be learned ABC DEF");
 
 /** If not using nodemon
  * transpile JavaScript tsc
